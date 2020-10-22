75a5dbf2f2e8a404d342bd09c22f7223da6aa907
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2020-10-23 04:43:13 +0600

    Add files via upload

---
+++ FSQRT.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x;
+        cin>>x;
+        cout<<(int)sqrt(x)<<endl;
+    }
+}
