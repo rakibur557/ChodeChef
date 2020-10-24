cf4e33545e7cd780f11100b5b84e28415093eb43
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2020-10-24 06:49:35 +0600

    Add files via upload

---
+++ DIFFSUM.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int f,s;
+	cin >> f >> s;
+	if(f>s){
+	    cout << f-s <<endl;
+	}
+	else
+	    cout<<f+s<<endl;
+
+	return 0;
+}
+
