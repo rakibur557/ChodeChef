cc3cef33d99c90b18e8cc28abff085ecca3e7f9a
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2020-10-24 06:19:51 +0600

    Add files via upload

---
+++ FLOW010.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string ch;
+        cin>>ch;
+
+        if(ch=="B" || ch=="b")
+        {
+            cout<<"BattleShip"<<endl;
+        }
+        else if(ch=="C" || ch=="c")
+        {
+            cout<<"Cruiser"<<endl;
+        }
+        else if(ch=="D" || ch=="d")
+        {
+            cout<<"Destroyer"<<endl;
+        }
+        else if(ch=="F" || ch=="f")
+        {
+            cout<<"Frigate"<<endl;
+        }
+    }
+    return 0;
+}
