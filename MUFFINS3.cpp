9f907be7894ac22c139ba6695fe2a7587fb69059
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2020-10-27 13:02:04 +0600

    Add files via upload

---
+++ MUFFINS3.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int numOfCake;
+        cin>>numOfCake;
+
+        cout<<(numOfCake/2)+1<<endl;
+    }
+
+
+    return 0;
+}
